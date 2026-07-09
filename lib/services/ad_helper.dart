7be0b95fe942510473d327382af49a43a47a3d2f
dhavalkumarg-iprogramer <dhavalkumarg@iprogrammer.co>
2026-07-09 13:08:12 +0530

    added live ads key

--- lib/services/ad_helper.dart
+++ lib/services/ad_helper.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if(Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';   //test
-      // return 'ca-app-pub-9085836508167933/5314203719';   //live
+      // return 'ca-app-pub-3940256099942544/6300978111';   //test
+      return 'ca-app-pub-9085836508167933/5314203719';   //live
     } else if(Platform.isIOS) {
       return 'ca-app-pub-9085836508167933/1727146643';
     } else {
@@ -14,8 +14,8 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if(Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';   //test
-      // return 'ca-app-pub-9085836508167933/4194774725';   //live
+      // return 'ca-app-pub-3940256099942544/1033173712';   //test
+      return 'ca-app-pub-9085836508167933/4194774725';   //live
     } else if(Platform.isIOS) {
       return 'ca-app-pub-9085836508167933/8873185093';
     } else {
@@ -25,8 +25,8 @@ class AdHelper {
 
   static String get rewardedAdUnitId {
     if(Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/5224354917";   //test
-      // return "ca-app-pub-9085836508167933/8282158150";   //live
+      // return "ca-app-pub-3940256099942544/5224354917";   //test
+      return "ca-app-pub-9085836508167933/8282158150";   //live
     } else if(Platform.isIOS) {
       return 'ca-app-pub-9085836508167933/7474297346';
     } else {
